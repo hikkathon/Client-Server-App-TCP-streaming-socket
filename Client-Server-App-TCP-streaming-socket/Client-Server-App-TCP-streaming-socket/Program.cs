a9632e4668d61f8787530948c4dcb6beb631a5bf
hikkathon <hikkathon@gmail.com>
2020-08-05 20:13:55 +0700

    create project

---
+++ Client-Server-App-TCP-streaming-socket/Client-Server-App-TCP-streaming-socket/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Client_Server_App_TCP_streaming_socket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
